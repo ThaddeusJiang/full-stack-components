86fcf3a9e39e76bc8bf653315709c31ba7bd8726
TJ <thaddeusjiang@gmail.com>
2023-01-20 08:21:54 +0900

    Template Engine: Pug

--- order_Pug/routes/index.js
+++ order_Pug/routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/room', function (req, res, next) {
+  res.render('room', { title: 'Room', room: { name: 'test', user: 'jiang' } });
+});
+
+router.post('/room', function (req, res, next) {
+  res.render('room', {
+    title: 'Room',
+    room: {
+      name: req.body.name,
+      user: req.body.user,
+    },
+  });
 });
 
 module.exports = router;
